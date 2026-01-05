8f7c51cb088206da667c87db17618fe841758ea0
Alfa <128333121+Alfatihahrizqullaab@users.noreply.github.com>
2026-01-05 11:10:00 +0700

    Perbarui

--- app_lapangan_futsal/lib/screen/profile_screen.dart
+++ app_lapangan_futsal/lib/screen/profile_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app_lapangan_futsal/screen/profile_page.dart';
+import 'package:app_lapangan_futsal/screen/detail_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:app_lapangan_futsal/service/encrypt.dart';
